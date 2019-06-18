b2a0e6fb7b357d3b640ce055aa04ca72d8249c57
MartinTonev <martin.e.tonev@gmail.com>
2019-06-18 03:56:41 +0200

    Initial commit

---
+++ buildScreen.js
@@ -0,0 +1,31 @@
+    var numberRowkeys = ['1', '2', '3','4', '5', '6', '7', '8', '9', '0', '-', '='];
+    var topRowkeys = ['Q', 'W','E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\'];
+    var midRowkeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\''];
+    var bottomRowkeys = ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.','/'];
+    var keys = [numberRowkeys,topRowkeys,midRowkeys,bottomRowkeys];
+
+
+    
+    keys.forEach(row => {
+        var rowHtml = document.createElement('div');
+        rowHtml.className = 'row justify-content-center';
+        row.forEach(keys => {
+            var keysHtml = document.createElement('div');
+            keysHtml.className = 'col-auto';
+            var key = document.createElement('a');
+            key.id = keys;
+            key.className = 'btn btn-outline-secondary';
+            key.innerHTML = keys;
+
+            key.addEventListener('keyup',function(){
+                key.style.borderColor = "#000000";
+                key.style.boxShadow = "0 0 10px #000000";
+            });
+
+            keysHtml.style = 'width: 15px; margin: 10px';
+            keysHtml.appendChild(key);
+            rowHtml.appendChild(keysHtml);
+        });
+        document.getElementsByClassName("content")[0].appendChild(rowHtml);
+    });
+   
---
+++ controls.js
@@ -0,0 +1,104 @@
+var keys = ['1', '2', '3','4', '5', '6', '7', '8', '9', '0', '-', '=',
+'Q', 'W','E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\',
+'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'',
+'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.','/'];
+
+var active = 0;
+var running = false;
+
+function keyStuff(event) {
+
+    var x = event.charCode || event.keyCode;  // Get the Unicode value
+    var y = String.fromCharCode(x);  // Convert the value into a character
+
+    /*  The above 2 lines of code come from:
+    http://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_event_key_charcode4 */
+    confirm(y);
+
+}
+function start(){ 
+    var startButton = document.getElementById('start');
+    startButton.className = "btn btn-outline-danger";
+    startButton.innerHTML = "Stop";
+
+    var interval = document.getElementById('seconds').value;
+    //console.log(interval);
+    var remainingLives = 3;
+    var lives = document.getElementById('livesAmount');
+    lives.innerHTML = remainingLives;
+
+   
+    if(running){      
+        startButton.className = "btn btn-outline-secondary";
+                startButton.innerHTML = "Start";
+        running = false;
+        clearInterval(game);
+        console.log(running);
+    }else{
+        running = true;
+        var game = setInterval(() =>{
+            if(remainingLives > 0 && running){
+                getRandomKey();
+                console.log(running);
+            }else{
+              
+                startButton.className = "btn btn-outline-secondary";
+                startButton.innerHTML = "Start";
+                running = false;
+                clearInterval(game);
+                console.log(running);
+            }         
+        },interval);
+    }
+
+    setInterval(() =>{
+        if(active <= 3){
+
+        }else{
+            var text = document.getElementById('lives');
+            this.backgroundColor = "#FF0000";
+            
+            text.style.borderColor = "#FF0000";
+            text.style.boxShadow = "0 0 10px #FF0000";
+            setTimeout(function(){
+                this.backgroundColor = "#FFFFFF";
+                text.style.borderColor = "#707070";
+                text.style.boxShadow = "0 0 0px #FF0000";
+            },500);
+            remainingLives--;
+            lives.innerHTML = remainingLives;
+            active = 0;
+            clearKeys();
+        }
+    },100);
+}
+function confirm(y){
+    var button = document.getElementById(y.toUpperCase());
+    button.style.backgroundColor = 'transparent';
+    button.style.borderColor = "#707070";
+    button.style.boxShadow = "0 0 0px #FF0000";
+    active--;
+}
+function getRandomKey(){
+    var currentKey = keys[Math.floor(Math.random()*keys.length)];
+    LightUpKey(currentKey);
+    active++;
+}
+function LightUpKey(currentKey){
+    var button = document.getElementById(currentKey);
+    button.style.backgroundColor = "#FF0000";
+    button.style.borderColor = "#FF0000";
+    button.style.boxShadow = "0 0 10px #FF0000";
+}
+function clearKeys(){
+    keys.forEach(key => {
+        var button = document.getElementById(key);
+        button.style.backgroundColor = 'transparent';
+        button.style.borderColor = "#707070";
+        button.style.boxShadow = "0 0 0px #FF0000";
+    });
+}
+function getDifficulty(){
+    var message = document.getElementById('message');
+    message.innerHTML = "Game speed: " + document.getElementById('seconds').value/1000 + " second per key";
+}
